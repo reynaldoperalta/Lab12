73062b8fa0d770f2245d4a18d1edf6e4a58adfb0
reynaldoperalta <reynaldopema14@gmail.com>
2021-11-02 12:15:12 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+ 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      //home: HomePage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        //'alert': (context) => AlertPage(),
+        //'avatar': (context) => AvatarPage(),
+      },
+    ) ;
+  }
+}
